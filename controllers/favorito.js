25b05f948ab4df98f64414253c4b3c1f5f62f61d
ivanmuinos <imuinos@uade.edu.ar>
2017-09-16 18:40:02 -0300

    Add files via upload

---
+++ controllers/favorito.js
@@ -0,0 +1,119 @@
+'use strict'
+
+var Favorito = require('../models/favorito');
+
+function prueba(req, res){
+	
+	if(req.params.nombre){
+		var nombre = req.params.nombre;
+	}else{
+		var nombre = "SIN NOMBRE";
+	}
+	
+
+	res.status(200).send({
+					data: [2,3,4],
+					message: "Tuvieja con nodeJS y EXPRESS" + " - " + nombre
+					});
+
+}
+
+function getFavorito(req, res){
+	var favoritoId = req.params.id;
+
+	Favorito.findById(favoritoId, function(err, favorito){
+		if(err){
+			res.status(500).send({message: 'Error al devolver los marcadores'});
+		}
+		if(!favorito){
+			res.status(404).send({message: 'No hay marcadores'});
+		}else{
+			res.status(200).send({favorito});
+		}
+		
+	});	
+
+}
+
+
+function getFavoritos(req, res){
+	Favorito.find({}).sort('-_id').exec((err, favoritos) => {
+		if(err){
+			res.status(500).send({message: 'Error al devolver los marcadores'});
+		}else{
+			if (!favoritos){
+				res.status(404).send({message: 'No hay marcadores'});
+			}else{
+				res.status(200).send({favoritos});
+			}
+		}
+	});
+	
+}
+
+
+function saveFavorito(req, res){
+	var favorito = new Favorito();
+
+	var params = req.body;
+	
+	favorito.title = params.title;
+	favorito.description = params.description;
+	favorito.url = params.url;
+
+	favorito.save((err, favoritoStored) => {
+		if(err){
+			res.status(500).send({message: 'Error al guardar el marcador'});
+		}else{
+			res.status(200).send({favorito: favoritoStored});
+		}
+	});
+
+}
+
+function updateFavorito(req, res){
+	var favoritoId = req.params.id;
+	var update = req.body;
+
+	Favorito.findByIdAndUpdate(favoritoId, update, (err, favoritoUpdated) =>{
+		if(err){
+			res.status(500).send({message: 'Error al guardar el marcador'});
+		}else{
+			res.status(200).send({favorito: favoritoUpdated});
+		}
+	});
+	
+}
+
+function deleteFavorito(req, res){
+	var favoritoId = req.params.id;
+
+	Favorito.findById(favoritoId, function(err, favorito){
+		if(err){
+			res.status(500).send({message: 'Error al devolver los marcadores'});
+		}
+		if(!favorito){
+			res.status(404).send({message: 'No hay marcadores'});
+		}else{
+			favorito.remove(err => {
+				if(err){
+					res.status(500).send({message: 'Error al borrar'});	
+				}else{
+					res.status(200).send({message: 'El marcador se ha eliminado'});
+				}
+			});
+		}
+		
+	});	
+	
+}
+
+module.exports = {
+	prueba,
+	getFavorito,
+	getFavoritos,
+	saveFavorito,
+	updateFavorito,
+	deleteFavorito
+}
+
